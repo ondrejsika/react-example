e33046a0ab17a495d90925fdfd8feaa90dc6585f
Ondrej Sika <ondrej@ondrejsika.com>
2019-06-28 22:05:44 +0200

    Add static build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {};
+module.exports = {
+  exportPathMap: async function(defaultPathMap) {
+    return {
+      '/': { page: '/' },
+    };
+  }
+};
 
 const withPages = require('@primer/next-pages/plugin')
 module.exports = withPages(module.exports)
