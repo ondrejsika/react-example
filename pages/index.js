985a5817ae6ee3678d4dbbae90abbf95f4bd6a6e
Ondrej Sika <ondrej@ondrejsika.com>
2019-07-16 17:00:12 +0200

    Add images using next-images

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { Box } from "@primer/components";
 import Hello from '../components/Hello'
 
+import nela_img from "../data/nela.jpg";
+
 function Home() {
   return <>
     <Box width={[1/2]}>
       <Hello name="Zuz" />
+      <img src={require('../data/nela.jpg')} />
+      <img src={nela_img} />
     </Box>
   </>;
 }
