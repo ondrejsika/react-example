e87a8492447dc90bc7a6f4570a0f7744606b5d0b
Ondrej Sika <ondrej@ondrejsika.com>
2019-08-10 01:01:14 +0200

    Add JSON import example

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Hello from "../components/Hello";
 
 import nela_img from "../data/nela.jpg";
+import json_file from "../data/example.json";
 
 function Home() {
   return (
@@ -8,6 +9,12 @@ function Home() {
       <Hello name="Zuz" />
       <img src={require("../data/nela.jpg")} />
       <img src={nela_img} />
+      <h2>JSON Data</h2>
+      <ul>
+        {json_file.map((el, i) => {
+          return <li key={i}>{el}</li>;
+        })}
+      </ul>
     </>
   );
 }
